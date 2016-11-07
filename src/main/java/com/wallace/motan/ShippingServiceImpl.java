a43d0a83c86219a8881988a9dda8f8bdee7bcb8d
Wallace Wan <wallace.wan@sap.com>
2016-11-07 16:47:33 +0800

    Output: group v1

--- src/main/java/com/wallace/motan/ShippingServiceImpl.java
+++ src/main/java/com/wallace/motan/ShippingServiceImpl.java
@@ -4,6 +4,6 @@ public class ShippingServiceImpl implements ShippingService {
 	
 	public String hello(String name) {
         System.out.println(name + " invoked shipping service");
-        return "Hello " + name + ", it's greeting from shipping service";
+        return "Hello " + name + ", it's greeting from shipping service (group v1)";
     }
 }
